874de44017fcfc95c76d540d1d7c5304d34ea4d3
Farid Ansari <farid12ansari7@gmail.com>
2021-12-26 20:01:57 +0530

    api urls

---
+++ src/config/api_urls.js
@@ -0,0 +1,10 @@
+const ApiUrls = {
+  agents: 'getlistofagents',
+  durationRange: 'getdurationrange',
+  filteredCalls: 'getfilteredcalls',
+  calls: 'getcalllist',
+  labels: 'getlistoflabels',
+  applyLabels: 'applyLabels',
+};
+
+export default ApiUrls;
