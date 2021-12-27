34e4603623086cc8e64bfc7c496e46e6743a8f7b
Farid Ansari <farid12ansari7@gmail.com>
2021-12-28 00:05:49 +0530

    added dependencies
    - react-icons
    - uuid

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,11 +4,14 @@ import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 
 import Home from 'src/scenes/home/home';
 
+import CallLabels from './scenes/call_labels/call_labels';
+
 function Routes() {
   return (
     <Switch>
+      <Route path="/call_labels" component={CallLabels} />
       <Route path="/home" component={Home} />
-      <Redirect to="/home" />
+      <Redirect to="/call_labels" />
     </Switch>
   );
 }
