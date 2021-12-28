3d9c745b13fd549da65f1728eca0793ecb147b05
Farid Ansari <farid12ansari7@gmail.com>
2021-12-28 16:33:05 +0530

    navbar, logo

--- src/shared_ui/layout/nav_bar.jsx
+++ src/shared_ui/layout/nav_bar.jsx
@@ -4,10 +4,17 @@ import { NavLink } from 'react-router-dom';
 
 import Styles from './layout.module.scss';
 
+const LOGO_URL =
+  'https://uploads-ssl.webflow.com/5fcb3bbf6fdac54bdd345a59/5fcb3bbf6fdac505ce345d09_prodigal%20white.svg';
+
 function NavBar() {
   return (
     <nav className={Styles.navBarWrapper}>
-      <div>Logo</div>
+      <div className={Styles.logoWrapper}>
+        <div className={Styles.logo}>
+          <img src={LOGO_URL} alt="Prodigal" />
+        </div>
+      </div>
       <div>
         <NavLink
           tabIndex={-1}
